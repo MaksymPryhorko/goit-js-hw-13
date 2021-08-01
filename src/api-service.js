2a0ec3ed7575b28f0fcb8f15ca01aa4d9da25441
MaksymPryhorko <78167917+MaksymPryhorko@users.noreply.github.com>
2021-08-01 08:08:12 +0300

    hw 13 on fetches - done.

---
+++ src/api-service.js
@@ -0,0 +1,57 @@
+import Notiflix from 'notiflix';
+
+export default class ApiService {
+    constructor() {
+        this.searchValue = '';
+        this.page = 1;
+        this.amountData = 0;
+    }
+
+    fetchArticles() {
+        const API_KEY = 'https://pixabay.com/api/?key=22710862-ad31ee603fc8e39b27d5b9240';
+        const searchParameters = `&q=${this.searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+
+        return fetch(`${API_KEY}${searchParameters}`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            };
+
+            throw new Error(response.status)
+        })
+        .then(images => {
+            if (images.hits.length === 0) {
+                Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+                return images;
+            };
+            this.incrementPage();
+            return images;
+        })
+        .catch(error => console.log(error))
+    }
+
+    incrementPage() {
+        this.page += 1;
+    };
+
+    resetPage() {
+        this.page = 1;
+    };
+
+    set query(newQuery) {
+        this.searchValue = newQuery;
+    };
+
+    set amount(newAmountData) {
+        this.amountData = newAmountData;
+    };
+
+    incrementAmountData() {
+        this.amountData += 40;
+    };
+
+    resetAmountData() {
+        this.amountData = 0;
+    }
+};
+
---
+++ src/index.js
@@ -0,0 +1,68 @@
+import './css/styles.css';
+import galleryTpl from './gallery.hbs'
+import ApiService from './api-service'
+import Notiflix from 'notiflix';
+
+
+const searchFormRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+const buttonLoadMoreRef = document.querySelector('.load-more');
+
+const apiService = new ApiService();
+
+searchFormRef.addEventListener('submit', onSearch);
+buttonLoadMoreRef.addEventListener('click', fetchData);
+
+function clearMarup() {
+    galleryRef.innerHTML = '';
+};
+
+function paintMarkup(hits) {
+    const imagesList = hits.map(galleryTpl).join('');
+    galleryRef.insertAdjacentHTML('beforeend', imagesList);
+};
+
+function onSearch(event) {
+    event.preventDefault();
+    const inputValue = event.currentTarget.searchQuery.value;
+
+    if (inputValue.trim() === '') {
+        Notiflix.Notify.warning('Sorry, but search input not be empty.');
+        return;
+    };
+
+    clearMarup();
+    apiService.query = inputValue;
+    apiService.resetPage();
+    apiService.resetAmountData();
+    fetchData();
+};
+
+function fetchData() {
+    hideButtonLoadMore()
+    apiService.fetchArticles().then((data) => {
+        if (data.hits.length === 0) {
+            return;
+        };
+        apiService.incrementAmountData();
+        paintMarkup(data.hits);
+        console.log(apiService.amountData);
+        if (apiService.amountData >= data.totalHits) {
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+            return;
+        };
+
+        displayButtonLoadMore();
+    });
+};
+
+function hideButtonLoadMore() {
+    buttonLoadMoreRef.classList.add('is-hidden');
+};
+
+function displayButtonLoadMore() {
+    buttonLoadMoreRef.classList.remove('is-hidden');
+};
+
+
+
